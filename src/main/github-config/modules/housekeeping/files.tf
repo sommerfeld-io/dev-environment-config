1d42cad889a2f611faec57c27ea5238404d9df7f
sebastian <sebastian@sommerfeld.io>
2023-05-26 22:34:02 +0200

    manage files centrally

--- src/main/github-config/modules/housekeeping/files.tf
+++ src/main/github-config/modules/housekeeping/files.tf
@@ -6,12 +6,3 @@ resource "github_repository_file" "files_gitignore" {
   file                = ".gitignore"
   content             = file("${path.module}/assets/files/.gitignore")
 }
-
-resource "github_repository_file" "files_gitpod" {
-  repository          = var.repo_name
-  branch              = "main"
-  commit_message      = "[Actions Bot] Gitpod update"
-  overwrite_on_create = true
-  file                = ".gitpod.yml"
-  content             = file("${path.module}/assets/files/.gitpod.yml")
-}
