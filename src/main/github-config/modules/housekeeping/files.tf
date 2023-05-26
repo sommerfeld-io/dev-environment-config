85e16dd9163578a04237653554d22b7bccf326e1
sebastian <sebastian@sommerfeld.io>
2023-05-26 22:45:53 +0200

    don't manage linters from terraform

--- src/main/github-config/modules/housekeeping/files.tf
+++
@@ -1,8 +0,0 @@
-resource "github_repository_file" "files_gitignore" {
-  repository          = var.repo_name
-  branch              = "main"
-  commit_message      = "[Actions Bot] .gitignore update"
-  overwrite_on_create = true
-  file                = ".gitignore"
-  content             = file("${path.module}/assets/files/.gitignore")
-}
